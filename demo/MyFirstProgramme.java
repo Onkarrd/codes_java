485e47ae60b9d88e1f13edcf9548b3a8540737dc
odeshpande <odeshpande@qualys.com>
2023-01-06 23:52:00 +0530

    new code

---
+++ demo/MyFirstProgramme.java
@@ -0,0 +1,13 @@
+package demooo;
+
+public class MyFirstProgramme {
+
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 40; i++) {
+      sb.append("*");
+      System.out.println(sb);
+    }
+  }
+
+}
